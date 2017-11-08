dc57f7b72e1ecf28d5a00ace68598c294381d111
cike111 <1134916885@qq.com>
2017-11-08 10:40:19 +0800

    Initial Commit

---
+++ CarthageFXShare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CarthageFXShare
+//
+//  Created by huangwei on 2017/11/8.
+//  Copyright © 2017年 huangwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
